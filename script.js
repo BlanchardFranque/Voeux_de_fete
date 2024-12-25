f665c3f0b208cc6f46f9fc2764a3b4792b1abc6f
BlanchardFranque <bkabala967@gmail.com>
2024-12-25 11:23:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const messages = {
         "message": "Avec tout mon cœur, je vous souhaite des fêtes remplies de joie et de bonheur, Votre Serviteur Franque Blanchard.",
         "buttonText": "Cliquez Ici Pour Écouter Le Message",
         "audioSrc": "voeux.mp3", // Fichier audio en français
-        "imageMessage": "Mes vœux les meilleurs pour vous"
+        "circleMessage": "Mes vœux les meilleurs pour vous"
     },
     "en": {
         "greeting": "🎄 Merry Christmas 2024 and Happy New Year 2025, Dear All! 🎉",
         "message": "With all my heart, I wish you holidays full of joy and happiness! All the best in your life.",
         "buttonText": "Click Here To Listen To The Message",
         "audioSrc": "voeu.mp3", // Fichier audio en anglais
-        "imageMessage": "My Wishings to you"
+        "circleMessage": "My Wishings to you"
     }
 };
 
@@ -21,19 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const defaultLanguage = "fr"; // Français par défaut
     setLanguage(defaultLanguage);
     document.getElementById('lang').value = defaultLanguage;
-
-    // Lecture automatique de l'audio au premier clic
-    const playButton = document.getElementById('playAudio');
-    playButton.addEventListener('click', () => {
-        const audioElement = document.getElementById('voeuxAudio');
-        if (audioElement.paused) {
-            audioElement.play();
-            playButton.textContent = "Pause";
-        } else {
-            audioElement.pause();
-            playButton.textContent = messages[defaultLanguage].buttonText;
-        }
-    });
 });
 
 // Fonction pour changer la langue
@@ -52,11 +39,6 @@ function setLanguage(language) {
     const audioElement = document.getElementById('voeuxAudio');
     audioElement.src = messages[language].audioSrc;
 
-    // Mettre à jour le message autour de l'image
-    document.getElementById('image-message').innerText = messages[language].imageMessage;
-
-    // Ajuster le texte du bouton si l'audio est en pause
-    if (audioElement.paused) {
-        document.getElementById('playAudio').textContent = messages[language].buttonText;
-    }
+    // Mettre à jour le message circulaire
+    document.getElementById('circle-message').innerText = messages[language].circleMessage;
 }
